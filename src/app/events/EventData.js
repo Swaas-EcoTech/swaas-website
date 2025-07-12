0c25f0628518edab85111582af394db60e537971
AbbasAliNaqvi <abbasalinaqvi888@gmail.com>
2025-07-12 21:41:10 +0530

    Add Events page with EventCard component and event data

---
+++ src/app/events/EventData.js
@@ -0,0 +1,43 @@
+const eventsData = {
+    "Upcoming Events": [
+        {
+            date: "3rd",
+            month: "November",
+            year: "2023",
+            title: "Loren Ipsum",
+            description:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            imageUrl: "/placeholder.svg?height=200&width=200",
+        },
+        {
+            date: "3rd",
+            month: "November",
+            year: "2023",
+            title: "Loren Ipsum",
+            description:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            imageUrl: "/placeholder.svg?height=200&width=200",
+        },
+    ],
+    "Past Events": [
+        {
+            date: "3rd",
+            month: "November",
+            year: "2023",
+            title: "Loren Ipsum",
+            description:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            imageUrl: "/placeholder.svg?height=200&width=200",
+        },
+        {
+            date: "3rd",
+            month: "November",
+            year: "2023",
+            title: "Loren Ipsum",
+            description:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            imageUrl: "/placeholder.svg?height=200&width=200",
+        },
+    ]
+};
+export default eventsData;
---
+++ src/app/events/page.jsx
@@ -0,0 +1,77 @@
+'use client';
+import  { useState } from 'react';
+import eventsData from './EventData.js';
+import EventCard from '../components/EventsCard.jsx';
+import Navbar from '../components/Navbar.jsx';
+
+
+const Events = () => {
+  const [selectedEvent, setSelectedEvent] = useState("Upcoming Events");
+
+
+  return (
+    <div>
+      <Navbar />
+    <div className="pageBackground">
+      <div className="teamContainer">
+        <img
+          src='/image-5.png'
+          alt="Decorative Leaf"
+          className={`leafDecoration leafTopRight`}
+        />
+        <img
+          src='/image-5.png'
+          alt="Decorative Leaf"
+          className={`leafDecoration leafBottomLeft`}
+        />
+
+        <div className={`diamondDecoration diamond1`}></div>
+        <div className={`diamondDecoration diamond2`}></div>
+        <div className={`diamondDecoration diamond3`}></div>
+        <div className={`diamondDecoration diamond4`}></div>
+
+        <header className={`teamHeader`}>
+          <h1>Events</h1>
+        </header>
+
+        <nav className={`categoryNav`}>
+          {Object.keys(eventsData).map((eventType) => (
+            <button
+              key={eventType}
+              className={`categoryButton ${selectedEvent === eventType ? 'active' : ''
+                }`}
+              onClick={() => setSelectedEvent(eventType)}
+            >
+              {eventType}
+            </button>
+          ))}
+          <div className={`wavyLine`}>
+            <img src={'/img1.png'} alt="Wavy line decoration" />
+          </div>
+        </nav>
+
+        <div className={`eventsContainer`}>
+          <EventCard />
+          <EventCard />
+          <EventCard />
+        </div>
+
+
+        
+      </div>
+      <img
+        src={'/image-14.png'}
+        alt="Bottom Left Decoration"
+        className={`bottomLeftImage`}
+      />
+      <img
+        src={'/image-10.png'}
+        alt="Bottom Right Decoration"
+        className={`bottomRightImage`}
+      />
+    </div>
+    </div>
+  );
+}
+
+export default Events
